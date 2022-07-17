b22b7a076a33015a53610a155d0781d33ae68479
fufylev <fufylev@gmail.com>
2022-07-17 15:31:00 +0300

    Update recursive_sorting_test.dart

--- test/lessons/recursion/recursive_sorting_test.dart
+++ test/lessons/recursion/recursive_sorting_test.dart
@@ -7,14 +7,14 @@ void main() {
   group('Сортировки рекурсией', () {
     // Arrange
     final unsorted = [1, 7, 0, 2, 4, 8, 9, 5, 3, 4, 7, 10, 6, 11];
-    final sorted = [0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9, 10, 11];
+    final sortedGiven = [0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9, 10, 11];
 
     test('Рекурсивная сортировка слиянием для массивов чисел', () {
       // Act
       final sorted = sortByMerge(unsorted);
 
       // Assert
-      expect(sorted, sorted);
+      expect(sorted, sortedGiven);
     });
 
     test('Рекурсивная сортировка массива чисел методом сортировки Тони Хоара ', () {
@@ -22,7 +22,7 @@ void main() {
       final sorted = sortByTonyHoare(unsorted);
 
       // Assert
-      expect(sorted, sorted);
+      expect(sorted, sortedGiven);
     });
   });
 }
